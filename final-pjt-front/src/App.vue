6fc95a4630334f10bfc39e94f8b970fa73a67355
Felix0942364 <jaewoongcho93@gmail.com>
2023-05-18 17:59:28 +0900

    login & signup page added

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -8,16 +8,36 @@
       <div class="nav-list">
         <router-link to="/search">Search</router-link> 
         <router-link to="/community">Community</router-link> 
-        <router-link to="/watchlists">Watchlist</router-link> 
+        <router-link to="/watchlists">My Watchlists</router-link> 
       </div>
       <div class="nav-profile">
-        <router-link to="/profile">Profile</router-link> 
+        <div v-if="this.$store.getters.isAuthenticated">
+          <router-link to="/profile">Profile</router-link> 
+          <p @click="logOut">Log Out</p> 
+        </div>
+        <div v-else>
+          <router-link to="/login">Log In</router-link>
+          <router-link to="/signup">Sign Up</router-link>
+        </div>
       </div>
     </nav>
     <br>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default ({
+  name:'app',
+  methods: {
+    logOut() {
+      this.$store.dispatch('logOut')
+    }
+  }
+})
+</script>
+
+
 
 <style src="@/assets/style_sheet.css">
 </style>
--- final-pjt-front/src/views/LoginView.vue
+++ final-pjt-front/src/views/LoginView.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="login">
+    <h1>LogIn Page</h1>
+    <form @submit.prevent="logIn">
+      <label for="username">username : </label>
+      <input type="text" id="username" v-model="username"><br>
+
+      <label for="password"> password : </label>
+      <input type="password" id="password" v-model="password"><br>
+
+      <input type="submit" value="logIn">
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'LoginView',
-  components: {
+  data() {
+    return {
+      username: "",
+      password: "",
+    }
+  },
+  methods: {
+    logIn() {
+      const username = this.username
+      const password = this.password
+      
+      const payload = {
+        username, password
+      }
+      this.$store.dispatch('logIn', payload)
+    }
   }
 }
 </script>
