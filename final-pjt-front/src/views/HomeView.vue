5bea3db3bf6761c46df82cf09e1821fe49495268
HidenLee <hyundo1995@gmail.com>
2023-05-19 10:03:23 +0900

    inital Setting for WYSIWYG

--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/img/logo.png">
+  <div>
+    <div class="Home-head">
+      <img alt="Vue logo" src="@/assets/img/logo.png">
+    </div>
+    <div class="Home-body">
+      <div class="Trending">
+
+      </div>
+      <div class="Popular_Year">
+
+      </div>
+      <div class="Latest">
+
+      </div>     
+    </div> 
+    <!-- <wysywyg v-model="myHTML"/> -->
   </div>
 </template>
 
