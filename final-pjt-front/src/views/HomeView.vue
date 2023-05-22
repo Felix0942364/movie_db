2e7ea59ff751e78021335b4bf430dfc4fb9006ba
HidenLee <hyundo1995@gmail.com>
2023-05-22 09:01:53 +0900

    apply CKEDITOR to article CREATEFORM

--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -14,16 +14,22 @@
 
       </div>     
     </div> 
-    <!-- <wysywyg v-model="myHTML"/> -->
+
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-
+// import CKEDITOR from 'https://cdn.ckeditor.com/ckeditor5/37.1.0/super-build/ckeditor.js'
 export default {
   name: 'HomeView',
   components: {
-  }
+  },
+  // mounted(){
+  //   let recaptchaScript = document.createElement('script')
+  //   recaptchaScript.setAttribute('src', 'https://cdn.ckeditor.com/ckeditor5/37.1.0/super-build/ckeditor.js')
+  //   document.head.appendChild(recaptchaScript)
+  // }
 }
 </script>
+
+
