83656e1417c4c08f612a5b6cacfab9ddb433de50
HidenLee <hyundo1995@gmail.com>
2023-05-22 17:58:26 +0900

    0522HD_Homeview making

--- final-pjt-front/src/views/HomeView.vue
+++ final-pjt-front/src/views/HomeView.vue
@@ -5,16 +5,33 @@
     </div>
     <div class="Home-body">
       <div class="Trending">
-
+        <!-- MovieCards -->
+        <div class="popular-list row row-cols-1 row-cols-md-5 gy-3">
+          <MovieCard
+            v-for="(movie, idx) in TrendingList"
+            :key="idx"
+            :movie="movie"/>
+        </div>
       </div>
       <div class="Popular_Year">
-
+        <!-- MovieCards -->
+        <div class="popular-list row row-cols-1 row-cols-md-5 gy-3">
+          <MovieCard
+            v-for="(movie, idx) in TrendingList"
+            :key="idx"
+            :movie="movie"/>
+        </div>
       </div>
       <div class="Latest">
-
+        <!-- MovieCards -->
+        <div class="popular-list row row-cols-1 row-cols-md-5 gy-3">
+          <MovieCard
+            v-for="(movie, idx) in TrendingList"
+            :key="idx"
+            :movie="movie"/>
+        </div>
       </div>     
     </div> 
-
   </div>
 </template>
 <script>
@@ -24,11 +41,25 @@ export default {
   name: 'HomeView',
   components: {
   },
-  // mounted(){
-  //   let recaptchaScript = document.createElement('script')
-  //   recaptchaScript.setAttribute('src', 'https://cdn.ckeditor.com/ckeditor5/37.1.0/super-build/ckeditor.js')
-  //   document.head.appendChild(recaptchaScript)
-  // }
+  // data(){
+  //   return{
+  //     TrendingList : []
+  //   }
+  // },
+  methods:{
+    getMovies(){
+      this.$store.dispatch('getMovies')
+    }
+  },
+  computed:{
+    TrendingList(){
+      return this.$store.state.movies
+    }
+  },
+  created(){
+    this.getMovies()
+  }
+
 }
 </script>
 
