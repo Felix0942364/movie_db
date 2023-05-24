7e8042a6e4ab37c49b1aef55f6796a9f6e04fe4d
felix0942364 <jaewoongcho93@gmail.com>
2023-05-24 09:16:05 +0900

    may 24

--- final_pjt_back/accounts/serializers.py
+++ final_pjt_back/accounts/serializers.py
@@ -3,7 +3,8 @@ from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
-    
+    # article_set = 아티클 셋, 코멘트셋, watchlist 불러서 알고리즘 작성할 것
+    # 알고리즘 작성은 backend에서 진행 예정
     class Meta:
         model = get_user_model()
         fields = ('id', 'username', 'profile_img', 'profile_message', 'followers', 'preferences')
