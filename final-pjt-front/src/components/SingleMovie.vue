711e50a19b4ad5c1113af9c6ad8b43c19de8d575
HidenLee <hyundo1995@gmail.com>
2023-05-26 00:33:59 +0900

    infinitescrole_1

--- final-pjt-front/src/components/SingleMovie.vue
+++ final-pjt-front/src/components/SingleMovie.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="movie-item col-2 mb-3" @click="godetail">
+  <div class="movie-item col-lg-2 col-md-3 col-sm-4 mb-3" @click="godetail">
     <img
     :src="`https://image.tmdb.org/t/p/w500${movie.poster_path}`" class="img-fluid"/>
   </div>
@@ -26,9 +26,7 @@ img{
   aspect-ratio: 1/2
 }
 .movie-item {
-  /* position: relative; */
-  /* display: block; */
-  /* flex: 1 1 0px; */
+
   transition: transform 500ms;
 }
 
@@ -36,19 +34,10 @@ img{
   cursor: pointer;
 }
 
-.movie-item:focus,
+/* .movie-item:focus, */
 .movie-item:hover {
   transform: scale(1.4);
   z-index: 1;
 }
 
-.popular-list:focus-within ~ .movie-item,
-.popular-list:hover ~ .movie-item {
-  transform: translateX(-25%);
-}
-
-.movie-item:focus  .movie-item,
-.movie-item:hover  .movie-item {
-  transform: translateX(25%);
-}
 </style>
--- final-pjt-front/src/views/SearchView.vue
+++ final-pjt-front/src/views/SearchView.vue
@@ -6,27 +6,32 @@
     </form>
     <div class="my-3">
       <h1 v-if="query===null&&searchInput===null">검색어를 입력하시오~~!</h1>
-      <h1 v-if="query">{{ query }}에 대한 검색 결과 {{ movies.length }} 건 <button @click="query=null;movies=[]">X</button></h1>
+      <h1 v-if="query"><span style="text-decoration: underline;">{{ query }}</span>에 대한 검색 결과 {{ movies.length }} 건 <button @click="query=null;movies=[]">X</button></h1>
     </div>
-    <div class="container gy-3 mx-5 mt-5">
+    <div class="container gy-3 mx-auto mt-5">
       <div class="row">
         <SingleMovie
           v-for="movie in movies"
           :key="movie.id"
           :movie="movie"
-          />    
+          />
+          <infinite-loading @infinite="infiniteHandler">
+            <div slot="no-more" style="color: rgb(102, 102, 102); font-size: 14px; padding: 10px 0px;">목록의 끝입니다 :)</div>
+          </infinite-loading> 
       </div>
-      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import InfiniteLoading from 'vue-infinite-loading'
 import axios from 'axios'
 import SingleMovie from '@/components/SingleMovie.vue'
 export default {
   name: 'SearchView',
   components: {
-    SingleMovie
+    SingleMovie,
+    InfiniteLoading
   },
   data() {
     return {
@@ -36,6 +41,9 @@ export default {
     }
   },
   methods: {
+    infiniteHandler(){
+      console.log(123)
+    },
     search() {
       const TMDB_KEY = process.env.VUE_APP_TMDB_KEY
       this.query = this.searchInput
