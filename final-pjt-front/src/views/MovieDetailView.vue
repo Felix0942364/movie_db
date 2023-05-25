8c73371307331d9f76f1c33a3509d53e45b095cc
HidenLee <hyundo1995@gmail.com>
2023-05-26 08:14:00 +0900

    ?.notation

--- final-pjt-front/src/views/MovieDetailView.vue
+++ final-pjt-front/src/views/MovieDetailView.vue
@@ -38,7 +38,7 @@
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
-                <h1 class="modal-title fs-5 text-secondary" id="exampleModalLabel">{{$store.state.user.username}}님의 재생목록</h1>
+                <h1 class="modal-title fs-5 text-secondary" id="exampleModalLabel">{{$store.state.user?.username}}님의 재생목록</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
